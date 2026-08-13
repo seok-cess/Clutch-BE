02206acda66dd6ddb34318b66ae23cdb17b90346
jeahong <burindol3@naver.com>
2026-08-13 10:53:00 +0900

    feat: UserCouponRepository 작성

---
+++ src/main/java/com/clutch/wallet/repository/UserCouponRepository.java
@@ -0,0 +1,14 @@
+package com.clutch.wallet.repository;
+
+import com.clutch.wallet.domain.UserCoupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
+
+    boolean existsByClaimId(Long claimId);
+
+    Optional<UserCoupon> findByClaimId(Long claimId);
+
+}
